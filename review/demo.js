6aee8ef3eecff4457def77e693d25275399ba0ad
Ayushsingh0203 <61348173+Ayushsingh0203@users.noreply.github.com>
2021-02-17 09:58:10 +0530

    Delete review directory

--- review/demo.js
+++
@@ -1,9 +0,0 @@
-let dwarves = ['bifur', 'bofur', 'bombur', 'fili', 'kili', 'oin', 'gloin', 'thorin', 'balin', 'dwalin', 'nori', 'dori'];
-
-
-let nameLengths = dwarves.map(function(item,index, array){
-
-  return item.length;
-});
-
-console.log(nameLengths);
